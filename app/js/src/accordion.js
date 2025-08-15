738f8b0d561e0ae00346fe9939af4fd75ba44ad5
Иванов Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-08-15 19:11:13 +0500

    add

--- app/js/src/accordion.js
+++
@@ -1,8 +0,0 @@
-// !!!!!!!!!!!!переписать без jquery, jquery удален из сборки!!!!!!
-
-$(document).ready(function () {
-    $('.partner-tags__list > li').click(function (e) {
-        $(".partner-tags__list > li").not($(this)).removeClass("active").find(".answer").slideUp();
-        $(this).toggleClass("active").find(".answer").slideToggle();
-    });
-});
