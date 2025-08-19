a50188a2eca6ed41e83d31f75c8aa798d155e47a
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-08-19 17:48:09 +0500

    add

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     if (e.target.classList.contains('btn-copy')) {
         const widgetBox = e.target.closest('.widget-box');
         if (!widgetBox) return;
@@ -16,4 +16,29 @@ document.addEventListener('click', function(e) {
             console.error('Ошибка копирования:', err);
         });
     }
+});
+
+// выделение
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll('.header__container a');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // убираем активность у всех ссылок
+            navLinks.forEach(link => link.classList.remove("active"));
+
+            // находим ссылку по href
+            const id = entry.target.getAttribute("id");
+            const activeLink = document.querySelector(`.header__container a[href="#${id}"]`);
+            if (activeLink) activeLink.classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.6 // срабатывает, если 60% секции видно
+});
+
+// подключаем наблюдатель к секциям
+sections.forEach(section => {
+    observer.observe(section);
 });
